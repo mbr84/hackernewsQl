df51c143361cac5285a821d1e4e4c1a4ab9eaa6e
mbr84 <matthew.bramfeld@gmail.com>
2017-08-06 05:43:56 -0700

    updating the cache/store

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { GC_USER_ID } from '../constants'
 import { timeDifferenceForDate } from '../utils'
+import { gql, graphql } from 'react-apollo'
 
 class Link extends Component {
 
@@ -22,9 +23,46 @@ class Link extends Component {
   }
 
   _voteForLink = async () => {
-    // ... you'll implement this in chapter 6
+    const userId = localStorage.getItem(GC_USER_ID)
+    const voterIds = this.props.link.votes.map(vote => vote.user.id)
+    if (voterIds.includes(userId)) {
+      console.log(`User (${userId}) already voted for this link.`)
+      return
+    }
+
+    const linkId = this.props.link.id
+    await this.props.createVoteMutation({
+      variables: {
+        userId,
+        linkId
+      },
+      update: (store, { data: { createVote } }) => {
+        this.props.updateStoreAfterVote(store, createVote, linkId)
+      }
+    })
   }
 
 }
 
-export default Link
+const CREATE_VOTE_MUTATION = gql`
+  mutation CreateVoteMutation($userId: ID!, $linkId: ID!) {
+    createVote(userId: $userId, linkId: $linkId) {
+      id
+      link {
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`
+
+export default graphql(CREATE_VOTE_MUTATION, {
+  name: 'createVoteMutation'
+})(Link)
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,6 +3,14 @@ import { graphql, gql } from 'react-apollo'
 import Link from './Link'
 
 class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: ALL_LINKS_QUERY })
+
+    const votedLink = data.allLinks.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+
+    store.writeQuery({ query: ALL_LINKS_QUERY, data })
+  }
 
   render() {
 
@@ -19,20 +27,35 @@ class LinkList extends Component {
     return (
       <div>
         {linksToRender.map((link, idx) => (
-          <Link key={link.id} link={link} index={idx} />
+          <Link
+            key={link.id}
+            link={link}
+            index={idx}
+            updateStoreAfterVote={this._updateCacheAfterVote}
+          />
         ))}
       </div>
     )
   }
 }
 
-const ALL_LINKS_QUERY = gql`
+export const ALL_LINKS_QUERY = gql`
   query AllLinksQuery {
     allLinks {
       id
       createdAt
       url
       description
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
     }
   }
 `
